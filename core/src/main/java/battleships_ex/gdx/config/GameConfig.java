3fca259d395a54758378c2a2e3e584639397fd71
Supattra Wongsamal <supattrw@stud.ntnu.no>
2026-03-06 15:38:34 +0100

    add game config for screensize

---
+++ core/src/main/java/battleships_ex/gdx/config/GameConfig.java
@@ -0,0 +1,8 @@
+package battleships_ex.gdx.config;
+
+public final class GameConfig {
+    private GameConfig() {}
+
+    public static final float WORLD_WIDTH = 450f;
+    public static final float WORLD_HEIGHT = 800f;
+}
