e38e34dcbd6b2bb7015d8bb99c449c71b1c8c227
Yannic Neu <yannic@regionalverkehr.de>
2026-03-16 00:43:00 +0100

    Implement Coordinate Class

---
+++ core/src/main/java/battleships_ex/gdx/ui/Coordinate.java
@@ -0,0 +1,39 @@
+package battleships_ex.gdx.ui;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final int row;
+    private final int col;
+    public Coordinate(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException("Row and Column must be non-negative.");
+        }
+        this.row = row;
+        this.col = col;
+    }
+    public int getRow() {
+        return this.row;
+    }
+    public int getCol() {
+        return this.col;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinate)) return false;
+        Coordinate that = (Coordinate) o;
+        return row == that.row && col == that.col;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+            "row = " + row +
+            ", col = " + col +
+            "}";
+    }
+}
